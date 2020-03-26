30ee1c6f1bf5406674c7aa5ccb748a98fc554ebd
Enzo Diaz <enzodiazdev@gmail.com>
2020-03-26 16:57:53 -0300

    cors dev

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,7 @@
 const app = require("express")()
 const helmet = require("helmet")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 const {reqreslog} = require("../controllers/middlewares")
 
@@ -9,6 +10,7 @@ app.set("port", process.env.PORT || 3000)
 app.use(helmet())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use(reqreslog)
 

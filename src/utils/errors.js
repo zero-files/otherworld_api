fa8899b76f247685f0f6d8b31c52d1102d0a7c5f
Enzo Diaz <enzodiazdev@gmail.com>
2020-04-09 13:06:11 -0300

    minor changes

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,5 +1,5 @@
 const db = e => { 
-    console.error(e)
+    console.log(e)
     return {error: "Database error", message: "An error has occurred in the database, please try again."} 
 }
 const unexpected = {error: "Unexpected Error", message: "Something strange has happened in this request. Please try again later or contact the developer."}

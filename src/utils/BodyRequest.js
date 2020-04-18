b6e5296258f32d9ed2dbb3a113bc2e4b9636564e
Enzo Diaz <enzodiazdev@gmail.com>
2020-04-18 20:58:53 -0300

    clase para facilitar req.body

---
+++ src/utils/BodyRequest.js
@@ -0,0 +1,40 @@
+class BodyRequest {
+    constructor(...parameters) {
+        parameters.forEach(parameter => this[parameter] = null)
+    }
+
+    assign(body){
+        Object.keys(body).forEach(key => {
+            if (key in this) this[key] = body[key]
+        })
+    }
+
+    clear_null(){
+        Object.keys(this).forEach(parameter => {
+            if(this[parameter] === null) delete this[parameter]
+        })
+    }
+
+    has(param){
+        return this[`${param}`] ? true : false
+    }
+
+    get(param){
+        if(this.has(param)) return this[`${param}`]
+        else return undefined
+    }
+
+    delete(param){
+        if(this.has(param)) delete this[`${param}`]
+    }
+
+    is_empty(){
+        return Object.keys(this).length ? true : false
+    }
+
+    get parameters(){
+        return Object.assign({}, this)
+    }
+}
+
+module.exports = BodyRequest
